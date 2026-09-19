25452b9c95a6dc0ced0a6b9b58247ff8cc9ffe57
Rohan Kumar Sharma <138614626+RohanKumarSharma@users.noreply.github.com>
2026-09-19 23:38:33 +0530

    Time: 1 ms (78.23%) | Memory: 54.2 MB (86.19%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+     let a = 0;
+    let b = 0;
+
+    for (let i = 2; i <= cost.length; i++) {
+        let current = Math.min(
+            a + cost[i - 2],
+            b + cost[i - 1]
+        );
+
+        a = b;
+        b = current;
+    }
+
+    return b;
+};
